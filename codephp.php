c97c3ce98e0b37a8b37c488bb63ec3f6b2840838
sandesh6636 <134988883+sandesh6636@users.noreply.github.com>
2023-06-11 22:39:09 +0545

    Add files via upload

--- codephp.php
+++ codephp.php
@@ -1,56 +1,68 @@
 <?php
 session_start();
-require_once("conn.php");
 $msg = "";
 
+if (!isset($_SESSION['username'])) {
+  header("location: login.php");
+  exit();
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (!isset($_POST["email"]) || !isset($_POST["password"]) || !isset($_POST["cpassword"])) {
-        echo "All fields must be filled";
-    } else {
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $password = $_POST["password"];
-        $cpassword = $_POST["cpassword"];
-        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+  require_once("conn.php");
+  $task = $_POST['task'];
 
-        $email_qry = "SELECT * FROM login WHERE email = ?";
-        $stmt = mysqli_prepare($conn, $email_qry);
-        mysqli_stmt_bind_param($stmt, "s", $email);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_store_result($stmt);
-        $email_count = mysqli_stmt_num_rows($stmt);
+  // Get the logged-in user's ID from the session
+  $user_id = $_SESSION["id"];
 
-        $username_qry = "SELECT * FROM login WHERE username = ?";
-        $stmt = mysqli_prepare($conn, $username_qry);
-        mysqli_stmt_bind_param($stmt, "s", $username);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_store_result($stmt);
-        $username_count = mysqli_stmt_num_rows($stmt);
+  // Insert the task into the "tasks" table with the current timestamp
+  $insert_query = "INSERT INTO tasks (user_id, task, created_at) VALUES ('$user_id', '$task', CURRENT_TIMESTAMP)";
+  if (mysqli_query($conn, $insert_query)) {
+    $msg = "Task added successfully.";
+  } else {
+    $msg = "Error adding task: " . mysqli_error($conn);
+  }
+} elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['edit_task'])) {
+  require_once("conn.php");
+  $task_id = $_POST['task_id'];
+  $new_task = $_POST['new_task'];
 
-        if ($email_count) {
-            echo "Email Already Exists";
-        } elseif ($username_count) {
-            echo "Username Already Exists";
-        } else {
-            if ($password != $cpassword) {
-                echo "Password and Confirm Password do not match";
-            } else {
-                $qry = "INSERT INTO login(username, email, password) VALUES(?, ?, ?)";
-                $stmt = mysqli_prepare($conn, $qry);
-                mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashed_password);
-                $res = mysqli_stmt_execute($stmt);
-
-                if (!$res) {
-                    echo "Failed to register";
-                    echo mysqli_error($conn);
-                    die();
-                } else {
-                    $msg = "<div class='alert'>Registered Successfully</div>";
-                    header("location: login.php");
-                    exit();
-                }
-            }
-        }
-    }
+  // Update the task in the "tasks" table
+  $update_query = "UPDATE tasks SET task = '$new_task' WHERE id = '$task_id'";
+  if (mysqli_query($conn, $update_query)) {
+    $msg = "Task updated successfully.";
+  } else {
+    $msg = "Error updating task: " . mysqli_error($conn);
+  }
 }
 ?>
+
+<!-- Rest of your main page HTML code -->
+सन्देश
+सन्देश राई
+<?php
+session_start();
+$msg = "";
+
+if (!isset($_SESSION['username'])) {
+  header("location: login.php");
+  exit();
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  require_once("conn.php");
+  $task = $_POST['task'];
+
+  // Get the logged-in user's ID from the session
+  $user_id = $_SESSION["id"];
+
+  // Insert the task into the "tasks" table with the current timestamp
+  $insert_query = "INSERT INTO tasks (user_id, task, created_at) VALUES ('$user_id', '$task', CURRENT_TIMESTAMP)";
+  if (mysqli_query($conn, $insert_query)) {
+    $msg = "Task added successfully.";
+  } else {
+    $msg = "Error adding task: " . mysqli_error($conn);
+  }
+}
+?>
+
+<!-- Rest of your main page HTML code -->
