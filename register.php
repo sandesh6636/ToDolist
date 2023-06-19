01651fd82879771e70f1a8d3824af3737a9b68f7
sandesh <sital.nawa.8090@gmail.com>
2023-06-19 07:36:09 +0545

    update the home.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 require_once("conn.php");
-$email_msg = $username_msg = "";
+ $msg="";
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     if (!isset($_POST["email"]) || !isset($_POST["password"]) || !isset($_POST["cpassword"])){
         echo "all filds must be filled";
@@ -29,10 +29,16 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     
         if ($username_count) {
     
-          $username_msg = "<div class='alert '>Username Already Exist</div>";
+          $msg = '<div class="alert alert-danger alert-dismissible fade show font-weight-bold" role="alert">
+          <strong>User name already Exist</strong>
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
         }
          else if ($email_count) {
-          $email_msg = "<div class='alert '>Email Already Exist</div>";
+          $msg = '<div class="alert alert-danger alert-dismissible fade show font-weight-bold" role="alert">
+          <strong>Email already Exist</strong> 
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>';
         } else{
 
           
@@ -70,6 +76,11 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Registration Page</title>
   <link rel="stylesheet" href="static\css\register.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="static\css\login.css">
 </head>
 
 <body>
@@ -116,8 +127,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
       </div>
       <button type="submit">Register</button>
       <?php 
-        echo $username_msg; 
-       echo $email_msg;
+       echo $msg; 
       
       ?>
     </form>
